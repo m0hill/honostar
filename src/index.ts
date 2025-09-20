3b6732d26bfb27ba64454b0d6b3af48e57690519
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2025-09-20 19:13:23 +0900

    minor

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import '@/core/polyfills/compression.js'
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/bun'
 import { except } from 'hono/combine'
 import { compress } from 'hono/compress'
-
 import { logger } from 'hono/logger'
-import type { AppEnv } from '@/core/context'
-
-import { createSseEndpoint } from '@/core/datastar/endpoint'
-import { fxResponder } from '@/core/datastar/middleware'
-import { datastarResponder } from '@/core/datastar/responder'
-import { initContext } from '@/core/middleware'
-import { renderer } from '@/core/renderer'
-import { mountRoutes } from '@/core/router'
+import {
+  type AppEnv,
+  createSseEndpoint,
+  datastarResponder,
+  fxResponder,
+  initContext,
+  mountRoutes,
+  renderer,
+} from '@/core'
+import '@/core/polyfills/compression.js'
 
 const app = new Hono<AppEnv>()
 
