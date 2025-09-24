362e96ca8266f0c148b91be1e04781216f412cbe
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2025-09-24 18:38:09 +0900

    core utils

--- src/core/renderer.tsx
+++ src/core/renderer.tsx
@@ -23,7 +23,10 @@ export const renderer = factory.createMiddleware(async (c, next) => {
           <link rel="stylesheet" href="/styles.css" />
           <script type="module" src="/datastar.js" />
         </head>
-        <body data-on-load="@get('/_/events')" data-on-popstate__window="@get(location.pathname)">
+        <body
+          data-on-load="@get('/_/events')"
+          data-on-popstate__window="@get(location.pathname, { requestCancellation: 'disabled' })"
+        >
           <div id="app">{children}</div>
         </body>
       </html>
